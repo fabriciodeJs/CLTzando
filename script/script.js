747d4f6a17ee48060bf8db35ea3019d8a4fc8947
Fabricio de Jesus <114605554+fabriciodeJs@users.noreply.github.com>
2023-03-10 16:12:26 -0300

    Inicio do Projeto

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const saida_saidaDolar = document.querySelector('.dolar');
+const saida_euro = document.querySelector('.euro');
+const saida_ibovespa = document.querySelector('.ibovespa');
+const saida_bitcoin = document.querySelector('.bitcoin');
+
