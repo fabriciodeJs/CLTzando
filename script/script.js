31c5536851329f904314ce74557cf72262310958
Fabricio de Jesus <114605554+fabriciodeJs@users.noreply.github.com>
2023-03-13 16:37:20 -0300

    inicio calculo Salario liquido

--- script/script.js
+++ script/script.js
@@ -8,5 +8,68 @@ const saidaSalarioLiquido = document.querySelector('#resultadoSalario');
 const salarioBruto = document.querySelector('#valorSalario');
 const descontos = document.querySelector('#valorDescontos');
 const dependetes = document.querySelector('#valorDependetes');
+const btnSalario = document.querySelector('#btnSLiquido');
 
 
+
+btnSalario.addEventListener('click', (e) => {
+    let salario = Number.parseFloat(salarioBruto.value);
+    let desconto = Number.parseFloat(descontos.value);
+    let depedentes = Number.parseInt(dependetes.value)
+
+
+    const inss = verificacaoInss(salario);
+    calculoSalarioLiquido(salario, inss, depedentes, desconto);
+
+
+
+
+
+    salarioBruto.value = '';
+    descontos.value = '';
+    dependetes.value = '';
+});
+
+function calculoSalarioLiquido(salario, inss, depedentes, desconto) {
+
+    console.log(inss)
+    console.log(salario)
+    console.log(depedentes)
+    console.log(desconto)
+
+
+
+}
+
+function verificacaoInss(salario) {
+
+    if (salario <= 1302) return 7, 5;
+
+    if (salario <= 2571, 29) return 9;
+
+    if (salario <= 3856, 94) return 12;
+
+    if (salario <= 7507, 49) return 14;
+
+}
+String.prototype.reverse = function () {
+    return this.split('').reverse().join('');
+};
+
+function mascaraMoeda(campo, evento) {
+    let tecla = (!evento) ? window.event.keyCode : evento.which;
+    let valor = campo.value.replace(/[^\d]+/gi, '').reverse();
+    let resultado = "";
+    let mascara = "##.###.###,##".reverse();
+    for (var x = 0, y = 0; x < mascara.length && y < valor.length;) {
+        if (mascara.charAt(x) != '#') {
+            resultado += mascara.charAt(x);
+            x++;
+        } else {
+            resultado += valor.charAt(y);
+            y++;
+            x++;
+        }
+    }
+    campo.value = resultado.reverse();
+}
