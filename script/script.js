0b469bcdcad21fec68ff672ccedfe33dd8a9872a
Fabricio de Jesus <114605554+fabriciodeJs@users.noreply.github.com>
2023-03-13 11:28:20 -0300

    mudança de cores primaria

--- script/script.js
+++ script/script.js
@@ -1,5 +1,12 @@
+/*                  Moedas                */
 const saida_saidaDolar = document.querySelector('.dolar');
 const saida_euro = document.querySelector('.euro');
 const saida_ibovespa = document.querySelector('.ibovespa');
 const saida_bitcoin = document.querySelector('.bitcoin');
+/*                  Input                */
+const saidaSalarioLiquido = document.querySelector('#resultadoSalario');
+const salarioBruto = document.querySelector('#valorSalario');
+const descontos = document.querySelector('#valorDescontos');
+const dependetes = document.querySelector('#valorDependetes');
+
 
